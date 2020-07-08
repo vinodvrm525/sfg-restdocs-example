6798bfb015e6be152143fe7e87fc76b7b433022e
vinodvrm525 <vinodverma525@gmail.com>
2020-07-08 17:59:38 +0530

    adding pathParameters

---
+++ src/main/java/com/example/sfgrestdocsexample/web/mappers/BeerMapper.java
@@ -0,0 +1,16 @@
+package com.example.sfgrestdocsexample.web.mappers;
+
+import com.example.sfgrestdocsexample.domain.Beer;
+import com.example.sfgrestdocsexample.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by jt on 2019-05-25.
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDto BeerToBeerDto(Beer beer);
+
+    Beer BeerDtoToBeer(BeerDto dto);
+}
---
+++ src/test/java/com/example/sfgrestdocsexample/web/controller/BeerControllerTest.java
@@ -0,0 +1,92 @@
+package com.example.sfgrestdocsexample.web.controller;
+
+import com.example.sfgrestdocsexample.domain.Beer;
+import com.example.sfgrestdocsexample.repositories.BeerRepository;
+import com.example.sfgrestdocsexample.web.model.BeerDto;
+import com.example.sfgrestdocsexample.web.model.BeerStyleEnum;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
+@WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "com.example.sfgrestdocsexample.web.mappers")
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    BeerRepository beerRepository;
+
+    @Test
+    void getBeerById() throws Exception {
+        given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
+
+        mockMvc.perform(get("/api/v1/beer/{beerId}" , UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("v1/beer", pathParameters(
+                        parameterWithName("beerId").description("UUID of desired Beer to get")
+                )));
+    }
+
+    @Test
+    void saveNewBeer() throws Exception {
+        BeerDto beerDto =  getValidBeerDto();
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(RestDocumentationRequestBuilders.post("/api/v1/beer/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void updateBeerById() throws Exception {
+        BeerDto beerDto =  getValidBeerDto();
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(RestDocumentationRequestBuilders.put("/api/v1/beer/" + UUID.randomUUID().toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isNoContent());
+    }
+
+    BeerDto getValidBeerDto(){
+        return BeerDto.builder()
+                .beerName("Nice Ale")
+                .beerStyle(BeerStyleEnum.ALE.ALE)
+                .price(new BigDecimal("9.99"))
+                .upc(123123123123L)
+                .build();
+
+    }
+
+}
